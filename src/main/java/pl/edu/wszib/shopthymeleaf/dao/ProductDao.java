f4d79b96043392f6b086bebe6d33ca21702bcfb7
WolakMaciej <you@example.com>
2021-04-10 14:25:25 +0200

    wypełnienie danymi, serwis produktu

--- src/main/java/pl/edu/wszib/shopthymeleaf/dao/ProductDao.java
+++ src/main/java/pl/edu/wszib/shopthymeleaf/dao/ProductDao.java
@@ -1,7 +1,8 @@
 package pl.edu.wszib.shopthymeleaf.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.edu.wszib.shopthymeleaf.domain.Product;
-
+@Repository
 public interface ProductDao extends JpaRepository<Product,Long> {
 }
---
+++ src/main/java/pl/edu/wszib/shopthymeleaf/service/ProductService.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.shopthymeleaf.service;
+
+import pl.edu.wszib.shopthymeleaf.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> findAll();
+}
